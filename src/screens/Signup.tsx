8f24c6c9bb91b8642a958be648fdbc59dae7b0aa
Mariana <marianaribenova@hotmail.com>
2025-01-03 10:55:23 -0300

    Forms

--- src/screens/Signup.tsx
+++ src/screens/Signup.tsx
@@ -6,21 +6,47 @@ import {
   Text,
   VStack,
 } from '@gluestack-ui/themed'
+import { useForm, Controller } from "react-hook-form"
+import * as yup from "yup"
+import { yupResolver } from "@hookform/resolvers/yup"
 
 import { useNavigation } from '@react-navigation/native'
 import { AuthNavigatorRoutesProps } from '@routes/auth.routes'
 
 import BackgroundImg from '@assets/background.png'
 import Logo from '@assets/logo.svg'
+
 import { Input } from '@components/Input'
 import { Button } from '@components/Button'
 
+type FormDataProps = {
+  name: string
+  email: string
+  password: string
+  passwordConfirm: string
+}
+
+const signUpSchema = yup.object({
+  name: yup.string().required("Informe o nome"),
+  email: yup.string().required("Informe o e-mail").email("E-mail inválido"),
+  password: yup.string().required("Informe a senha").min(6, "A senha deve ter pelo menos 6 dígitos"),
+  passwordConfirm: yup.string().required("Confirme a senha").oneOf([yup.ref("password"), ""], "A confirmação da senha não confere")
+});
 
 export function SignUp() {
+
+  const { control, handleSubmit, formState: { errors } } = useForm<FormDataProps>({
+    resolver: yupResolver(signUpSchema),
+  });
+
   const navigator = useNavigation<AuthNavigatorRoutesProps>()
 
   function handleGoBack() {
     navigator.goBack()
+  }
+
+  function handleSignUp(data: FormDataProps) {
+    console.log(data)
   }
 
   return (
@@ -51,19 +77,71 @@ export function SignUp() {
           <Center flex={1} gap="$2">
             <Heading color="$gray100">Crie sua conta</Heading>
 
-            <Input placeholder="Nome" />
-
-            <Input
-              placeholder="E-mail"
-              keyboardType="email-address"
-              autoCapitalize="none"
+            <Controller
+              control={control}
+              name='name'
+              render={({ field: { onChange, value } }) => (
+                <Input
+                  placeholder="Nome"
+                  onChangeText={onChange}
+                  value={value}
+                  errorMessage={errors.name?.message}
+                />
+              )}
             />
-            <Input placeholder="Senha" secureTextEntry />
 
-            <Button title="Criar e acessar" />
+            <Controller
+              control={control}
+              name='email'
+              render={({ field: { onChange, value } }) => (
+                <Input
+                  placeholder="E-mail"
+                  keyboardType="email-address"
+                  autoCapitalize="none"
+                  onChangeText={onChange}
+                  value={value}
+                  errorMessage={errors.email?.message}
+                />
+              )}
+            />
+
+            <Controller
+              control={control}
+              name='password'
+              render={({ field: { onChange, value } }) => (
+                <Input
+                  placeholder="Senha"
+                  secureTextEntry
+                  onChangeText={onChange}
+                  value={value}
+                  errorMessage={errors.password?.message}
+                />
+              )}
+            />
+
+            <Controller
+              control={control}
+              name='passwordConfirm'
+              render={({ field: { onChange, value } }) => (
+                <Input
+                  placeholder="Confirme a senha"
+                  secureTextEntry
+                  onChangeText={onChange}
+                  value={value}
+                  onSubmitEditing={handleSubmit(handleSignUp)}
+                  returnKeyType='send'
+                  errorMessage={errors.passwordConfirm?.message}
+                />
+              )}
+            />
+
+            <Button
+              title="Criar e acessar"
+              onPress={handleSubmit(handleSignUp)}
+            />
           </Center>
 
-          <Button title="Voltar para o login" variant="outline" mt="$12" onPress={handleGoBack}/>
+          <Button title="Voltar para o login" variant="outline" mt="$12" onPress={handleGoBack} />
         </VStack>
       </VStack>
     </ScrollView>
