30548acd6e16834cacb3a36110ba601abdcd8ef9
Mariana <marianaribenova@hotmail.com>
2025-01-08 15:51:42 -0300

    consuming the database pt2

--- src/screens/Profile.tsx
+++ src/screens/Profile.tsx
@@ -2,34 +2,45 @@ import { useState } from "react";
 import { ScrollView, TouchableOpacity, KeyboardAvoidingView, Platform, TouchableWithoutFeedback, Keyboard } from "react-native";
 import { Center, VStack, Text, Heading, useToast } from "@gluestack-ui/themed";
 import { Controller, useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+
 import * as ImagePicker from "expo-image-picker"
 import * as FileSystem from "expo-file-system"
+import * as yup from "yup";
+
+import { useAuth } from "@hooks/useAuth";
 
 import { ScreeenHeader } from "@components/ScreenHeader";
 import { UserPhoto } from "@components/UserPhoto";
 import { Input } from "@components/Input";
 import { Button } from "@components/Button";
 import { ToastMessage } from "@components/ToastMessage";
-import { useAuth } from "@hooks/useAuth";
 
 type FormDataProps = {
     name: string;
-    email: string
-    password: string;
-    oldPassword: string;
-    passwordConfirm: string
+    email?: string;
+    password?: string;
+    old_password?: string;
+    confirm_password?: string;
 }
+
+const profileSchema = yup.object({
+    name: yup.string().required('Informe o nome.'),
+    password: yup.string().min(6, 'A senha deve ter pelo menos 6 dígitos').nullable().transform((value) => !!value ? value : null),
+    confirm_password: yup.string().nullable().transform((value) => !!value ? value : null).oneOf([yup.ref('password'), null], 'A confirmação de senha não confere.'),
+});
 
 export function Profile() {
     const [userPhoto, setUserPhoto] = useState("https://github.com/rnomaxy.png")
 
     const toast = useToast();
     const { user } = useAuth();
-    const { control } = useForm<FormDataProps>({
+    const { control, handleSubmit, formState: { errors } } = useForm<FormDataProps>({
         defaultValues: {
             name: user.name,
             email: user.email
-        }
+        },
+        resolver: yupResolver(profileSchema)
     });
 
     async function handleUserPhotoSelect() {
@@ -73,9 +84,13 @@ export function Profile() {
         }
     }
 
+    async function handleProfileUpdate(data: FormDataProps) {
+        console.log(data);
+    }
+
     return (
-        <KeyboardAvoidingView 
-            style={{ flex: 1 }} 
+        <KeyboardAvoidingView
+            style={{ flex: 1 }}
             behavior={Platform.OS === "ios" ? "padding" : "height"}
         >
             <TouchableWithoutFeedback onPress={() => Keyboard.dismiss()}>
@@ -111,6 +126,7 @@ export function Profile() {
                                             bg="$gray600"
                                             onChangeText={onChange}
                                             value={value}
+                                            errorMessage={errors.name?.message}
                                         />
                                     )}
                                 />
@@ -141,12 +157,54 @@ export function Profile() {
                             >
                                 Alterar senha
                             </Heading>
-                            <Center w="$full" gap="$4">
-                                <Input placeholder="Senha antiga" bg="$gray600" secureTextEntry />
-                                <Input placeholder="Nova senha" bg="$gray600" secureTextEntry />
-                                <Input placeholder="Confirme a nova senha" bg="$gray600" secureTextEntry />
 
-                                <Button title="Atualizar" />
+                            <Center w="$full" gap="$4">
+
+                                <Controller
+                                    control={control}
+                                    name='old_password'
+                                    render={({ field: { onChange } }) => (
+                                        <Input
+                                            placeholder="Senha antiga"
+                                            bg="$gray600"
+                                            secureTextEntry
+                                            onChangeText={onChange}
+                                        />
+                                    )}
+                                />
+
+                                <Controller
+                                    control={control}
+                                    name='password'
+                                    render={({ field: { onChange } }) => (
+                                        <Input
+                                            placeholder="Nova senha"
+                                            bg="$gray600"
+                                            secureTextEntry
+                                            onChangeText={onChange}
+                                            errorMessage={errors.password?.message}
+                                        />
+                                    )}
+                                />
+
+                                <Controller
+                                    control={control}
+                                    name='confirm_password'
+                                    render={({ field: { onChange } }) => (
+                                        <Input
+                                            placeholder="Confirme a nova senha"
+                                            bg="$gray600"
+                                            secureTextEntry
+                                            onChangeText={onChange}
+                                            errorMessage={errors.confirm_password?.message}
+                                        />
+                                    )}
+                                />
+
+                                <Button
+                                    title="Atualizar"
+                                    onPress={handleSubmit(handleProfileUpdate)}
+                                />
                             </Center>
                         </Center>
                     </ScrollView>
